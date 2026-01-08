e85a17dc6d48eb65f7fcce1b1d88a98957c2b751
raffi <raffiprasetyo06@gmail.com>
2026-01-08 20:27:55 +0700

    ubah judul form login

--- login.php
+++ login.php
@@ -36,7 +36,7 @@ if(isset($_SESSION['username'])){
           <div class="card-body">
            							<div class="text-center mb-3">
                 <i class="bi bi-person-circle h1 display-4"></i>
-                <p>My Daily Journal</p>
+                <p>Welcome To My Daily Journal</p>
                 <hr />
               </div>
               <form action="" method="post" id="loginform">
